3e14652d2c1c293a7129b9e83bc95e8f2fa2a4b0
Raja Jaisankar <rr2102598@gmail.com>
2021-03-05 15:23:24 +0530

    login

--- src/components/homecomp/header.jsx
+++ src/components/homecomp/header.jsx
@@ -4,48 +4,57 @@ import { BrowserRouter as Router} from "react-router-dom"
 
 
 function Header() {
+  
   return (
+  
     <div>
-        <Router forceRefresh={true}>
-          <Navbar bg="secondary" expand="lg" varient="dark">
-            <Navbar.Brand href="#home">Kinder</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto"></Nav>
-              <Form inline>
-                <Link to="/signup">
-                  <Button
-                    variant="outline-light"
-                    style={{ marginRight: "10px" }}
-                    // onClick={() => {
-                    //   // console.log("hello")
-                    //   // history.push("/signup")
-                    // }}
-                  >
-                    Sign Up
-                  </Button>
-                </Link>
-                <Link to="/login">
-                  <Button
-                    variant="outline-light"
-                    // onClick={() => {
-                    //   // console.log("hello")
-                    //   // history.push("/login")
-                    //   // history.push({
-                    //   //   pathname: "/login",
-                    //   //   customNameData: "hello",
-                    //   // })
-                    // }}
-                  >
-                    Login
-                  </Button>
-                </Link>
-                {/*             
+      <Router forceRefresh={true}>
+        <Navbar bg="secondary" expand="lg" varient="dark">
+          <Navbar.Brand href="#home">Kinder</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto"></Nav>
+            <Form inline>
+              <Link
+                to={{
+                  pathname: "/signup",
+                  state: {
+                    hello: true
+                  }
+                }}
+              >
+                <Button
+                  variant="outline-light"
+                  style={{ marginRight: "10px" }}
+                  // onClick={() => {
+                  //   // console.log("hello")
+                  //   // history.push("/signup")
+                  // }}
+                >
+                  Sign Up
+                </Button>
+              </Link>
+              <Link to="/login">
+                <Button
+                  variant="outline-light"
+                  // onClick={() => {
+                  //   // console.log("hello")
+                  //   // history.push("/login")
+                  //   // history.push({
+                  //   //   pathname: "/login",
+                  //   //   customNameData: "hello",
+                  //   // })
+                  // }}
+                >
+                  Login
+                </Button>
+              </Link>
+              {/*             
             <Login></Login>
              <Signup></Signup> */}
-              </Form>
-            </Navbar.Collapse>
-          </Navbar>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
       </Router>
     </div>
   )
